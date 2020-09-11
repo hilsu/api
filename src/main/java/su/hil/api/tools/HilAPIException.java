abfd4a5ff11df24fb9dd55b581ad370979580809
isKONSTANTIN <lokin135@mail.lokincompany.ru>
2020-09-11 19:20:21 +0300

    HilAPIException: мелкие правки

--- src/main/java/su/hil/api/tools/HilAPIException.java
+++ src/main/java/su/hil/api/tools/HilAPIException.java
@@ -2,12 +2,18 @@ package su.hil.api.tools;
 
 public class HilAPIException extends Exception {
 
+    protected APIResponse response;
+
     public HilAPIException(APIResponse response) {
         this("Unknown API error", response);
     }
 
     public HilAPIException(String message, APIResponse response) {
         super(message + ": '" + response.toString() + "'");
+        this.response = response;
     }
 
+    public APIResponse getResponse() {
+        return response;
+    }
 }
